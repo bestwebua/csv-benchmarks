cca4c6c2c1546992af9470d5b0bc6528a51fe69b
Vladislav Trotsenko <admin@bestweb.com.ua>
2024-11-21 08:13:54 +0100

    Technical/Refactor

--- tools/csv_file_generator.rb
+++ tools/csv_file_generator.rb
@@ -4,7 +4,7 @@
 require 'securerandom'
 
 class CsvFileGenerator
-  def self.generate(filename, rows, columns)
+  def self.call(filename, rows, columns)
     File.open(filename, 'w') do |file|
       file.puts((1..columns).to_a.join(','))
       (rows - 1).times { file.puts(Array.new(columns) { SecureRandom.hex(16) }.join(',')) }
@@ -12,4 +12,4 @@ class CsvFileGenerator
   end
 end
 
-CsvFileGenerator.generate(ARGV[0], ARGV[1].to_i, ARGV[2].to_i)
+CsvFileGenerator.call(ARGV[0], ARGV[1].to_i, ARGV[2].to_i)
